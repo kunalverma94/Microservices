e72e19c6c1ee6ffffc424776ad1c424a97cc1b46
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-02-04 10:59:00 +0530

    api init

---
+++ API1/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API1.Controllers
+{
+    [Route("")]
+    [Route("api/[controller]/[action]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [Route("")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Getwww()
+        {
+            return new string[] { "foo123 value1", "foo123 value2",Process.GetCurrentProcess().ProcessName };
+        }
+        [Route("[action]")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetIndex()
+        {
+            return new string[] { "Index value1", "Index value2" };
+        }
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Getwww()
+        //{
+        //    return new string[] { "www value1", "www value2" };
+        //}
+    }
+}
